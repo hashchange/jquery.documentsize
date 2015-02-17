ba0decd54431c89dafe68c5d1c768dce5c43b631
hashchange <heim@zeilenwechsel.de>
2015-02-17 13:54:17 +0100

    Added an AMD demo using Zepto

--- demo/amd/amd.js
+++ demo/amd/amd.js
@@ -3,11 +3,27 @@ requirejs.config( {
     baseUrl: '../../bower_components',
 
     paths: {
-        // Using a different jQuery here than elsewhere (1.x, instead of 2.x in node_modules).
-        // Makes the demo work in oldIE, too.
-        'jquery': '../demo/bower_demo_components/jquery/dist/jquery',
+        'zepto': '../demo/bower_demo_components/zepto/zepto',
         'jquery.documentsize': '/dist/amd/jquery.documentsize'
+    },
+
+    // Faking jQuery here, using Zepto instead.
+    //
+    // For parallel use of Zepto in supported browsers and jQuery in legacy ones, see
+    // - specifically: http://simonsmith.io/using-zepto-and-jquery-with-requirejs/#using-the-map-configuration
+    // - for background: http://requirejs.org/docs/jquery.html#noconflictmap for
+    map: {
+        '*': { 'jquery': 'zepto' }
+    },
+
+    // Zepto does not register as an AMD module, so we need to add to add a shim.
+    // See https://github.com/jrburke/r.js/issues/390#issuecomment-14276257
+    shim: {
+        zepto: {
+            exports: '$'
+        }
     }
+
 } );
 
 require( [
@@ -17,5 +33,15 @@ require( [
 
 ], function ( $ ) {
 
+    $( function () {
 
+        $( ".content" )
+            .append( $( '<p class="row"/>' ).text(
+                "The document width is " + $.documentWidth() + "px."
+            ) )
+            .append( $( '<p class="row"/>' ).text(
+                "The document height is " + $.documentHeight() + "px."
+            ) );
+
+    } );
 } );
